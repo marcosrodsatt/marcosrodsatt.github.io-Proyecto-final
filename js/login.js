74ae2f806770dde6ff9e78ad54ee6c602581e47b
manuelcoccaro <manuel.coccaro@gmail.com>
2025-08-21 20:54:11 -0300

    Agregado Login y Desafíate

---
+++ js/login.js
@@ -0,0 +1,39 @@
+const loginBox = document.getElementById("loginBox");
+    let timeout;
+
+    function mostrarLogin() {
+      loginBox.classList.add("show");
+      
+      // Reinicia el temporizador cada vez que se mueve el mouse
+      clearTimeout(timeout);
+      timeout = setTimeout(() => {
+        loginBox.classList.remove("show");
+      }, 6000); // Se oculta después de 3 segundos sin movimiento
+    }
+
+    document.addEventListener("mousemove", mostrarLogin);
+
+
+    document.addEventListener('DOMContentLoaded', function(){
+
+    let submitbttn = document.getElementById('submitbttn');
+
+    submitbttn.addEventListener('click', function(){
+      
+      let username = document.getElementById('username').value.trim();
+      let password = document.getElementById('password').value.trim();
+
+   
+    
+    if (username === "" || password === "") {
+      alert("Por favor completa todos los campos");
+      return;
+    }
+
+    // Guardar en sesión
+    sessionStorage.setItem("user", username);
+
+    // Redirigir
+    window.location.href = "index.html";
+  })
+  });
